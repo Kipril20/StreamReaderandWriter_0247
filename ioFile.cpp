67c172fb8d9a1d24a796ff4079a5eb3632257202
Kipril20 <rafky2004@gmail.com>
2025-06-09 17:43:33 +0700

    menulis dan memasukkan nilai dari objek ke dalam file

--- ioFile.cpp
+++ ioFile.cpp
@@ -23,3 +23,6 @@ int main()
         // Loop akan berhenti jika anda memasukkan karakter q
         if (baris == "q")
             break;
+        // menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
